bd4dab89b68cdee42088f05ea7f5b76fb1934308
namaomo <yellowleaf.linux@gmail.com>
2020-04-20 22:42:55 +0900

    リスト表示に変更

--- gui.sh
+++ gui.sh
@@ -21,37 +21,51 @@ case $? in
 *) zenity --error
 ;;
 esac
-
-zenity --question --text "Chromiumをインストールしますか？" 
-case $? in
-0) 
-sudo snap install chromium
-;;
-1) 
-;;
-*) zenity --error
-;;
-esac
-zenity --question --text "Firefoxをインストールしますか？" 
-case $? in
-0) 
+extension=$(zenity --list --title "インストールしたいブラウザを選択してください" --text="ブラウザの選択" --column "ブラウザ" Firefox Chromium Chrome Vivaldi インストールしない)
+case $extension in
+Firefox) 
 sudo apt install firefox
 ;;
-1) 
+Chromium) sudo snap install chromium
 ;;
-*) zenity --error
-;;
-esac
-zenity --question --text "Chromeをインストールしますか？" 
-case $? in
-0) 
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+Chrome) sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install google-chrome-stable
 ;;
+Vivaldi)
+wget https://downloads.vivaldi.com/stable/vivaldi-stable_2.2.1388.34-1_amd64.deb
+sudo dpkg -i vivaldi-stable_*.deb 
+;;
+インストールしない)
+;;
+esac
+zenity --question --text "複数ブラウザをインストールしますか？" 
+case $? in
+0) 
+extension=$(zenity --list --title "インストールしたいブラウザを選択してください" --text="ブラウザの選択" --column "ブラウザ" Firefox Chromium Chrome Vivaldi インストールしない)
+case $extension in
+Firefox) 
+sudo apt install firefox
+;;
+Chromium) sudo snap install chromium
+;;
+Chrome) sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+;;
+Vivaldi)
+wget https://downloads.vivaldi.com/stable/vivaldi-stable_2.2.1388.34-1_amd64.deb
+sudo dpkg -i vivaldi-stable_*.deb 
+;;
+esac
+;;
 1) 
 ;;
 *) zenity --error
 ;;
 esac
+zenity --info --title "成功" --text "セットアップが正常に完了しました。"
+
+
